8fa4e672781d532abc39b830ba500c38376cbbbc
Markus Staab <maggus.staab@googlemail.com>
2024-12-18 11:34:25 +0100

    Discard changes to src/AbstractPasswordExposedChecker.php

--- src/AbstractPasswordExposedChecker.php
+++ src/AbstractPasswordExposedChecker.php
@@ -69,7 +69,6 @@ abstract class AbstractPasswordExposedChecker implements PasswordExposedCheckerI
                     $cache->save($cacheItem);
                 }
             } catch (ClientExceptionInterface $e) {
-                var_dump($e->getMessage());
             }
         }
 
