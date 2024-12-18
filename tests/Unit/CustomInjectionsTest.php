a8487fe1eab79e259c1be835fd34d8711643771e
Markus Staab <maggus.staab@googlemail.com>
2024-12-18 12:26:58 +0100

    Use symfony/http-client (#3)
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

--- tests/Unit/CustomInjectionsTest.php
+++ tests/Unit/CustomInjectionsTest.php
@@ -41,14 +41,7 @@ class CustomInjectionsTest extends TestCase
         $pemFiles = glob(__DIR__.'/../../vendor/paragonie/certainty/data/*.pem');
         $bundle = new Bundle(end($pemFiles));
 
-        $cache = new CacheItemPool();
-        $cache->changeConfig(
-            [
-                'cacheDirectory'  => __DIR__.'/../../cache/dofilecache/',
-                'gzipCompression' => false,
-            ]
-        );
-
+        $cache = new FilesystemAdapter("password-exposed-cache");
         $passwordExposedChecker = new PasswordExposedChecker(null, $cache);
         $passwordExposedChecker->setBundle($bundle);
 
